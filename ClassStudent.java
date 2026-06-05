93c5a5f6209e01cfaa5b27ea5e6897c3cdf2568d
dakshinyasrees2025ece-sudo <dakshinyasree.s2025ece@sece.ac.in>
2026-06-05 11:10:00 +0400

    Add files via upload

--- ClassStudent.java
+++ ClassStudent.java
@@ -1,9 +1,16 @@
-
 public class ClassStudent {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
+		    String name;
+		    int rollNo;
+		    void display() {
+		        String college = "ABC Engineering College";
+		        System.out.println("Name: " + name);
+		        System.out.println("Roll No: " + rollNo);
+		        System.out.println("College: " + college);
+		    }
+		    public static void main(String[] args) {
+		        ClassStudent s = new ClassStudent();
+		        s.name = "Raj";
+		        s.rollNo = 101;
+		        s.display();
+		    }
+		}
