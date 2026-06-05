8d5f2261b15f7f68f3f39a7060d78facce5ba724
dakshinyasrees2025ece-sudo <dakshinyasree.s2025ece@sece.ac.in>
2026-06-05 11:14:30 +0400

    Update Circle.java

--- Circle.java
+++ Circle.java
@@ -1,9 +1,19 @@
+class Circle {
+    int radius;
 
-public class Circle {
+    void calculateArea() {
+        double pi = 3.14;      // Local variable
+        double area = pi * radius * radius;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        System.out.println("Radius = " + radius);
+        System.out.println("Area = " + area);
+    }
 
-	}
+    public static void main(String[] args) {
+        Circle c = new Circle();
 
+        c.radius = 7;          // Instance variable value
+
+        c.calculateArea();
+    }
 }
