4ea9a7c67937ede7cab8c1305d6e148a0c9f811b
dakshinyasrees2025ece-sudo <dakshinyasree.s2025ece@sece.ac.in>
2026-06-05 11:16:20 +0400

    Update Employee.java
    Refactored Employee class to include salary calculation.

--- Employee.java
+++ Employee.java
@@ -1,9 +1,16 @@
+class Employee {
+    int basicSalary;
+    void calculateSalary() {
+        int bonus = 5000;
+        int totalSalary = basicSalary + bonus;
+        System.out.println("Basic Salary: " + basicSalary);
+        System.out.println("Bonus: " + bonus);
+        System.out.println("Total Salary: " + totalSalary);
+    }
 
-public class Employee {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+    public static void main(String[] args) {
+        Employee emp = new Employee();
+        emp.basicSalary = 30000;
+        emp.calculateSalary();
+    }
 }
