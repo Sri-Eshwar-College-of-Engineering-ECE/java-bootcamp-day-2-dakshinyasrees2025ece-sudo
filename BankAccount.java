ca63b6463936ec5db1432b8c654a08bcced38a6b
dakshinyasrees2025ece-sudo <dakshinyasree.s2025ece@sece.ac.in>
2026-06-05 11:13:47 +0400

    Update BankAccount.java

--- BankAccount.java
+++ BankAccount.java
@@ -1,9 +1,19 @@
 
-public class BankAccount {
+class BankAccount {
+    int balance;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    void deposit() {
+        int depositAmount = 2000;
+        System.out.println("Current Balance: " + balance);
+        balance = balance + depositAmount;
+        System.out.println("Deposit Amount: " + depositAmount);
+        System.out.println("Updated Balance: " + balance);
+    }
 
-	}
-
+    public static void main(String[] args) {
+        BankAccount acc = new BankAccount();
+        acc.balance = 10000;
+        acc.deposit();
+    }
 }
+
